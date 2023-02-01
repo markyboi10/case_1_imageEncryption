ef1cb12b1c32fe4a2de41dcc63099f534f415f54
Mark Case <casem@merrimack.edu>
2023-01-31 22:15:15 -0500

    Store rgb values into array

---
+++ src/imageencryption/ImageEncryption.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package imageencryption;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.DataBuffer;
+import java.awt.image.DataBufferInt;
+import java.awt.image.DirectColorModel;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author Mark Case
+ */
+public class ImageEncryption {
+    
+    public static void encryption() throws IOException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        
+        // Variables
+        File inputFile = new File  ("C:\\Users\\Mark Case\\Pictures\\Saved Pictures\\tux.png"); //File to encrypt
+        Cipher cipher = null; // The cipher object.
+        KeyGenerator keyGen = null; // The AES keygenerator.
+        SecureRandom rand; // A secure random number generator.
+        byte[] rawIV = new byte[16]; // An AES init. vector.
+        IvParameterSpec iv; // The IV parameter for CBC. 
+    
+        try {
+            
+            BufferedImage image = ImageIO.read(inputFile); //reading in my input file
+            
+            // 1. Store rgb values into array
+            int w = image.getWidth(); // width
+            int h = image.getHeight(); // height
+            int total_pixels = (h*w);
+            Color[] colors = new Color[total_pixels];
+            int i = 0;
+            // Collect RGB pixel data with nested for-loop
+            for (int x = 0; x < w; x++) {
+                for (int y = 0; y < h; y++) {
+                    colors[i] = new Color(image.getRGB(x, y));
+                    i++;
+                } // end inner for-loop
+            } // end outer for-loop
+            
+            
+
+            
+        } catch (IOException ex) {
+            Logger.getLogger(ImageEncryption.class.getName()).log(Level.SEVERE, null, ex);
+        } // end try-catch
+        
+    
+    } // end encryption method
+    
+} // end ImageEncryption class
---
+++ src/imageencryption/Main.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package imageencryption;
+
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+
+/**
+ *
+ * @author Mark Case
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            try {
+                ImageEncryption.encryption();
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IllegalBlockSizeException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (BadPaddingException ex) {
+                Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (InvalidKeyException | IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
